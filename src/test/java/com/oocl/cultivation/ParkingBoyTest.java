092bc5d30171e7c50b5d609e07eec17005b5c49f
LAVADPR <lavadpr@oocl.com>
2020-10-16 14:25:07 +0800

    [Parking Boy][Prince] Test - Should return correct car when fetching given a correct ticket

--- src/test/java/com/oocl/cultivation/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/ParkingBoyTest.java
@@ -3,6 +3,7 @@ package com.oocl.cultivation;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
 
 
 class ParkingBoyTest {
@@ -18,16 +19,17 @@ class ParkingBoyTest {
         //then
         assertNotNull(ticket);
     }
-//    @Test
-//    void should_return_correct_car_when_fetching_given_a_correct_ticket() {
-//        //given
-//        Car car = new Car();
-//        ParkingBoy parkingBoy = new ParkingBoy(new ParkingLot());
-//
-//        //when
-//        ParkingTicket ticket = parkingBoy.park();
-//
-//        //then
-//        assertNotNull(ticket);
-//    }
+    @Test
+    void should_return_correct_car_when_fetching_given_a_correct_ticket() {
+        //given
+        Car car = new Car();
+        ParkingBoy parkingBoy = new ParkingBoy(new ParkingLot());
+        ParkingTicket parkingTicket = parkingBoy.park(car);
+
+        //when
+        Car fetchedCar = parkingBoy.fetch(parkingTicket);
+
+        //then
+        assertSame(car, fetchedCar);
+    }
 }
