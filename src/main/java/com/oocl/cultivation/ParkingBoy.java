0e58260b5e08e0ebad9b9925c96398082063bbae
LAVADPR <lavadpr@oocl.com>
2020-10-16 14:08:30 +0800

    [Parking Boy][Prince] Test - Should return parking ticket when parking given a car to parking boy

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -1,4 +1,12 @@
 package com.oocl.cultivation;
 
 public class ParkingBoy {
+
+    public ParkingBoy(ParkingLot parkingLot) {
+    }
+
+
+    public ParkingTicket park() {
+        return null;
+    }
 }
