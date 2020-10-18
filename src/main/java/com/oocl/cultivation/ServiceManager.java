e9f1be697a5de58018fc3ca500400e04ecd68f25
LAVADPR <lavadpr@oocl.com>
2020-10-18 16:30:46 +0800

    [Parking Boy][Prince] Refactor - Service Manager with no parking lot

--- src/main/java/com/oocl/cultivation/ServiceManager.java
+++ src/main/java/com/oocl/cultivation/ServiceManager.java
@@ -3,7 +3,7 @@ package com.oocl.cultivation;
 import java.util.List;
 
 public class ServiceManager extends ParkingBoy{
-    public List<ParkingBoy> parkingBoys;
+    private List<ParkingBoy> parkingBoys;
 
     public ServiceManager(List<ParkingLot> parkingLots, List<ParkingBoy> parkingBoys) {
         super(parkingLots);
@@ -11,15 +11,18 @@ public class ServiceManager extends ParkingBoy{
         parkingBoys.add(new ParkingBoy(parkingLots));
     }
 
-
     public ParkingTicket park(ParkingBoy parkingBoy, Car car) {
-        if(parkingBoys.stream().anyMatch(currentParkingBoy -> currentParkingBoy == parkingBoy)){
-            return parkingBoy.park(car);
-        }
-        throw new ParkingException("Parking boy not in management list");
+        throwExceptionIfParkingBoyNotInList(parkingBoy);
+        return parkingBoy.park(car);
     }
 
     public Car fetch(ParkingBoy parkingBoy, ParkingTicket parkingTicket) {
-        return null;
+        throwExceptionIfParkingBoyNotInList(parkingBoy);
+        return parkingBoy.fetch(parkingTicket);
+    }
+
+    private void throwExceptionIfParkingBoyNotInList(ParkingBoy parkingBoy){
+        if(parkingBoys.stream().noneMatch(currentParkingBoy -> currentParkingBoy == parkingBoy))
+            throw new ParkingException("Parking boy not in management list");
     }
 }
--- src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
@@ -4,6 +4,7 @@ import java.util.Comparator;
 import java.util.List;
 
 public class SuperSmartParkingBoy extends ParkingBoy {
+
     public SuperSmartParkingBoy(List<ParkingLot> parkingLots) {
         super(parkingLots);
     }
